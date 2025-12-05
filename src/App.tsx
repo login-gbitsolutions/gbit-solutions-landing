58b7046fcda33f3c7c79d1fde4acef6a46b65e94
Glauber Bronzato <glauber.bronzato@gbitsolutions.com.br>
2025-12-05 09:26:39 -0300

    ✨ Add service detail pages, remove social links, update footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Services from './components/Services'
@@ -5,9 +6,12 @@ import About from './components/About'
 import Technologies from './components/Technologies'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import ServiceAWS from './pages/ServiceAWS'
+import ServiceDev from './pages/ServiceDev'
+import ServiceMonitoring from './pages/ServiceMonitoring'
 import './App.css'
 
-function App() {
+function HomePage() {
   return (
     <div className="app">
       <Header />
@@ -20,6 +24,17 @@ function App() {
       </main>
       <Footer />
     </div>
+  )
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/servicos/aws" element={<ServiceAWS />} />
+      <Route path="/servicos/desenvolvimento" element={<ServiceDev />} />
+      <Route path="/servicos/monitoramento" element={<ServiceMonitoring />} />
+    </Routes>
   )
 }
 
